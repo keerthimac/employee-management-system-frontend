1ab6601c2dcdf20966013af749063fbb4a654c3f
keerthimac <keerthi.mac@gmail.com>
2025-03-06 14:20:47 +0530

    *add employee from initiated and tested. also tested with backend and it was successful

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmployeeComponent } from './page/manage-employee/manage-employee.component';
+import { AddEmployeeComponent } from './page/add-employee/add-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ManageEmployeeComponent, AddEmployeeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'employee_management_system_frontend';
---
+++ src/app/page/add-employee/add-employee.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-employee',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './add-employee.component.html',
+  styleUrl: './add-employee.component.css',
+})
+@Injectable({ providedIn: 'root' })
+export class AddEmployeeComponent {
+  constructor(private http: HttpClient) {}
+
+  public employee = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    roleId: '',
+    departmentId: '',
+  };
+
+  public firstName: string = '';
+
+  addEmployee() {
+    this.http
+      .post('http://localhost:8080/employee/add', this.employee)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+}
